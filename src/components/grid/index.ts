9c084ce827c56a21d3110bb15d32a9bebd69f81f
nmwei <254060001@qq.com>
2023-02-21 17:15:43 +0800

    网格布局

--- src/components/grid/index.ts
+++ src/components/grid/index.ts
@@ -4,6 +4,7 @@ import { Grid, GridItem } from './grid'
 
 export type { GridProps, GridItemProps } from './grid'
 
+//使用attachPropertiesToComponent将Item和Grid绑定在一起。
 export default attachPropertiesToComponent(Grid, {
   Item: GridItem,
 })
